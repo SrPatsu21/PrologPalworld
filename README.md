2306f94f144b6952a5c954a01301052a477ed019
SrPatsu21 <paulosergiopierdona@gmail.com>
2025-05-26 17:14:29 -0300

    reboot

--- README.md
+++ README.md
@@ -17,55 +17,71 @@ swipl -s palworld.pl
 ## COMO USAR
 
 ### Pesquisas
+
 **OBS: o jogo esta em desenvolvimento entao os resultados apresentados na documentacao podem ser diferentes dos reais mas o formato do retorno nao.**
 
 - Buscar pelo nome:
-    ```
+
+    ```sh
     buscar_por_nome(lamball, Numero, Tipo, Trabalhos, Montaria, Passivas).
     ```
+
     Numero = 1, Tipo = neutro, Trabalhos = [trabalho_manual, transporte, agricultura], Montaria = nao, Passivas = [fluffy_shield].
 
     ou voce pode evitar o retorno de alguns parametros substituindo eles por _.
 
-    ```
+    ```sh
     buscar_por_nome(lamball, Numero, Tipo, _, _, Passivas).
     ```
+
     Numero = 1, Tipo = [neutro], Passivas = [fluffy_shield].
 
 - Buscar por tipo:
-    ```
+
+    ```sh
     buscar_por_tipo(neutro, ListaNomes).
     ```
+
     ListaNomes = [lamball, cattiva, chikipi, direhowl].
 
 - Buscar pos lista de tipo:
-    ```
+
+    ```sh
     buscar_por_tipos([neutro], ListaNomes).
     ```
+
     ListaNomes = [lamball, cattiva, chikipi, direhowl]
 
 - Buscar por trabalho:
-    ```
+
+    ```sh
     buscar_por_trabalho(mineracao, ListaNomes).
     ```
+
     ListaNomes = [cattiva, rushoar, mammorest].
 
 - Buscar por trabalhos:
-    ```
+
+    ```sh
     buscar_por_trabalhos([transporte, corte], ListaNomes).
     ```
+
     ListaNomes = [mossanda, grizzbolt].
 
 - Buscar por montaria:
-    ```
+
+    ```sh
     buscar_por_montaria(ListaNomes).
     ```
+
     ListaNomes = [rushoar, direhowl, mossanda, galeclaw, kitsun, surfent, mammorest, grizzbolt, jetragon].
 
 - Busca por numero
+
+    ```sh
+        buscar_por_numero(103, Nome, Tipo, Trabalhos, Montaria, Passivas).
     ```
-    buscar_por_numero(103, Nome, Tipo, Trabalhos, Montaria, Passivas).
-    ```
+
     Nome = grizzbolt,
     Tipo = eletrico,
     Trabalhos = [geracao_eletricidade, trabalho_manual, transporte, corte],
@@ -74,7 +90,6 @@ swipl -s palworld.pl
 
 ### Iniciar o especialista em Pal
 
+```sh
+iniciar_especialista.
 ```
-especialista.
-```
-
--- palworld.pl
+++ palworld.pl
@@ -1,88 +1,148 @@
-% pals.pl - Sistema especialista para identificação de Pals
+% BASE DE DADOS
+% pal(Número, Nome, Tipos, Trabalhos, Montaria, Passivas).
+:- [base_palworld].
 
-:- dynamic yes/1, no/1.
+%-------------------------------------------------------------------------------------------------------------------------------%
+% Tipos e Trabalhos disponíveis
+tipos_possiveis([fogo, agua, grama, eletrico, terra, vento, dragao, neutro]).
+trabalhos_possiveis([trabalho_manual, transporte, agricultura, coleta, mineracao, plantio, corte, producao_medicinal, acendimento, geracao_eletricidade]).
 
-/* Entrada principal */
-iniciar :-
-    hipotese(Pal),
-    write('Eu acho que o Pal é: '),
-    write(Pal), nl,
-    undo.
+%-------------------------------------------------------------------------------------------------------------------------------%
+% Filtros
+buscar_por_nome(Nome, Numero, Tipo, Trabalhos, Montaria, Passivas) :-
+    pal(Numero, Nome, Tipo, Trabalhos, Montaria, Passivas).
 
-/* Hipóteses (Pals conhecidos) */
-hipotese(lamball) :- lamball, !.
-hipotese(cattiva) :- cattiva, !.
-hipotese(chikipi) :- chikipi, !.
-hipotese(lifmunk) :- lifmunk, !.
-hipotese(foxparks) :- foxparks, !.
-hipotese(desconhecido).  /* Se nenhuma hipótese for satisfeita */
+buscar_por_tipo(Tipo, ListaNomes) :-
+    findall(Nome, (pal(_, Nome, Tipos, _, _, _), member(Tipo, Tipos)), ListaNomes).
 
-/* Regras de identificação dos Pals */
-lamball :-
-    tipo(neutral),
-    trabalho([handiwork, transporting, farming]),
-    drop([wool, lamball_mutton]),
-    nao_montavel.
+buscar_por_tipos(ListaTipos, ListaNomes) :-
+    findall(Nome, (pal(_, Nome, Tipos, _, _, _), subset(ListaTipos, Tipos)), ListaNomes).
 
-cattiva :-
-    tipo(neutral),
-    trabalho([handiwork, gathering, mining, transporting]),
-    drop([red_berries]),
-    nao_montavel.
+buscar_por_trabalho(Trabalho, ListaNomes) :-
+    findall(Nome, (pal(_, Nome, _, Trabalhos, _, _), member(Trabalho, Trabalhos)), ListaNomes).
 
-chikipi :-
-    tipo(neutral),
-    trabalho([gathering, farming]),
-    drop([egg, chikipi_poultry]),
-    nao_montavel.
+buscar_por_trabalhos(ListaTrabalhos, ListaNomes) :-
+    findall(Nome, (pal(_, Nome, _, Trabalhos, _, _), subset(ListaTrabalhos, Trabalhos)), ListaNomes).
 
-lifmunk :-
-    tipo(grass),
-    trabalho([planting, handiwork, lumbering, medicine_production, gathering]),
-    drop([berry_seeds, low_grade_medical_supplies]),
-    nao_montavel.
+buscar_por_montaria(ListaNomes) :-
+    findall(Nome, pal(_, Nome, _, _, sim, _), ListaNomes).
 
-foxparks :-
-    tipo(fire),
-    trabalho([kindling]),
-    drop([leather, flame_organ]),
-    nao_montavel.
+buscar_por_numero(Numero, Nome, Tipo, Trabalhos, Montaria, Passivas) :-
+    pal(Numero, Nome, Tipo, Trabalhos, Montaria, Passivas).
 
-/* Atributos dos Pals */
-tipo(Tipo) :-
-    verificar(tipo_eh(Tipo)).
+%-------------------------------------------------------------------------------------------------------------------------------%
+% Predicado principal
+iniciar_especialista. :-
+    write('Pense em um Pal e eu tentarei adivinhar quem é.'), nl,
+    write('O Pal é montável? (sim/nao/nao_sei): '),
+    read(RespMontaria),
+    findall(Nome, pal(_, Nome, _, _, _, _), ListaPals),
+    filtrar_montaria(ListaPals, RespMontaria, PalsFiltrados),
+    tipos_possiveis(ListaTipos),
+    trabalhos_possiveis(ListaTrabalhos),
+    intercalar(ListaTipos, ListaTrabalhos, ListaPerguntas),
+    perguntar_caracteristicas(ListaPerguntas, PalsFiltrados, [], [], ResultadoFinal),
+    exibir_resultado(ResultadoFinal),
+    limpar_variaveis.
 
-trabalho(Lista) :-
-    forall(member(T, Lista), verificar(tem_trabalho(T))).
+%-------------------------------------------------------------------------------------------------------------------------------%
+% Auxiliares
+intercalar([], [], []).
+intercalar([H1|T1], [], [tipo-H1|Resto]) :- intercalar(T1, [], Resto).
+intercalar([], [H2|T2], [trabalho-H2|Resto]) :- intercalar([], T2, Resto).
+intercalar([H1|T1], [H2|T2], [tipo-H1, trabalho-H2|Resto]) :- intercalar(T1, T2, Resto).
 
-drop(Lista) :-
-    forall(member(D, Lista), verificar(drop_eh(D))).
+perguntar_caracteristicas([], Pals, _, _, Pals).
+perguntar_caracteristicas(_, [Unico], _, _, [Unico]) :-
+    write('Você está pensando em: '), write(Unico), nl,
+    write('Fim do jogo!'), nl.
 
-montavel :-
-    verificar(eh_montavel).
+perguntar_caracteristicas([tipo-Tipo|Resto], Pals, TiposConfirmados, TrabalhosConfirmados, ResultadoFinal) :-
+    length(TiposConfirmados, N),
+    ( N >= 2 ->
+        perguntar_caracteristicas(Resto, Pals, TiposConfirmados, TrabalhosConfirmados, ResultadoFinal)
+    ;
+        format('O Pal possui o tipo ~w? (sim/nao/nao_sei): ', [Tipo]),
+        read(Resposta),
+        (Resposta == sim ->
+            incluir_tipo(Pals, Tipo, PalsFiltrados),
+            append(TiposConfirmados, [Tipo], NovosTipos)
+        ; Resposta == nao ->
+            excluir_tipo(Pals, Tipo, PalsFiltrados),
+            NovosTipos = TiposConfirmados
+        ;
+            PalsFiltrados = Pals,
+            NovosTipos = TiposConfirmados
+        ),
+        perguntar_caracteristicas(Resto, PalsFiltrados, NovosTipos, TrabalhosConfirmados, ResultadoFinal)
+    ).
 
-nao_montavel :-
-    verificar(nao_montavel).
-
-/* Perguntas ao usuário */
-perguntar(Pergunta) :-
-    write('O Pal tem a seguinte característica: '),
-    write(Pergunta),
-    write('? (s/n) '),
+perguntar_caracteristicas([trabalho-Trabalho|Resto], Pals, TiposConfirmados, TrabalhosConfirmados, ResultadoFinal) :-
+    format('O Pal possui a habilidade de trabalho ~w? (sim/nao/nao_sei): ', [Trabalho]),
     read(Resposta),
-    nl,
-    ((Resposta == s ; Resposta == sim)
-     ->
-        assert(yes(Pergunta)) ;
-        assert(no(Pergunta)), fail).
+    (Resposta == sim ->
+        incluir_trabalho(Pals, Trabalho, PalsFiltrados),
+        append(TrabalhosConfirmados, [Trabalho], NovosTrabalhos)
+    ; Resposta == nao ->
+        excluir_trabalho(Pals, Trabalho, PalsFiltrados),
+        NovosTrabalhos = TrabalhosConfirmados
+    ;
+        PalsFiltrados = Pals,
+        NovosTrabalhos = TrabalhosConfirmados
+    ),
+    perguntar_caracteristicas(Resto, PalsFiltrados, TiposConfirmados, NovosTrabalhos, ResultadoFinal).
 
-/* Verificação com base no que já foi respondido */
-verificar(S) :-
-    (yes(S) -> true ;
-     no(S)  -> fail ;
-     perguntar(S)).
+%-------------------------------------------------------------------------------------------------------------------------------%
+% Funções auxiliares de filtragem
+filtrar_montaria(Pals, nao_sei, Pals).
+filtrar_montaria(Pals, RespMontaria, PalsFiltrados) :-
+    include({RespMontaria}/[Nome]>>pal(_, Nome, _, _, RespMontaria, _), Pals, PalsFiltrados).
 
-/* Limpa as respostas anteriores */
-undo :- retract(yes(_)), fail.
-undo :- retract(no(_)), fail.
-undo.
+tem_montaria(Resp, Nome) :-
+    pal(_, Nome, _, _, Resp, _).
+
+incluir_tipo(Pals, Tipo, PalsFiltrados) :-
+    include(tem_tipo(Tipo), Pals, PalsFiltrados).
+
+tem_tipo(Tipo, Nome) :-
+    pal(_, Nome, Tipos, _, _, _),
+    member(Tipo, Tipos).
+
+incluir_trabalho(Pals, Trabalho, PalsFiltrados) :-
+    include(tem_trabalho(Trabalho), Pals, PalsFiltrados).
+
+tem_trabalho(Trabalho, Nome) :-
+    pal(_, Nome, _, Trabalhos, _, _),
+    member(Trabalho, Trabalhos).
+
+excluir_tipo(Pals, Tipo, PalsFiltrados) :-
+    exclude(tem_tipo(Tipo), Pals, PalsFiltrados).
+
+excluir_trabalho(Pals, Trabalho, PalsFiltrados) :-
+    exclude(tem_trabalho(Trabalho), Pals, PalsFiltrados).
+
+%-------------------------------------------------------------------------------------------------------------------------------%
+% Resultado final
+exibir_resultado([]) :-
+    write('Não consegui encontrar um Pal com essas características.'), nl,
+    write('Fim do jogo!'), nl.
+exibir_resultado([Unico]) :-
+    write('Você está pensando em: '), write(Unico), nl,
+    write('Fim do jogo!'), nl.
+exibir_resultado(Pals) :-
+    sort(Pals, PalsOrdenados),
+    write('Os Pals que correspondem às características são: '), nl,
+    listar_pals(PalsOrdenados),
+    write('Fim do jogo!'), nl.
+
+listar_pals([]).
+listar_pals([H|T]) :-
+    write('- '), write(H), nl,
+    listar_pals(T).
+
+%-------------------------------------------------------------------------------------------------------------------------------%
+% Limpeza
+limpar_variaveis :-
+    (nb_current(tipos_confirmados, _) -> nb_delete(tipos_confirmados) ; true),
+    (nb_current(trabalhos_confirmados, _) -> nb_delete(trabalhos_confirmados) ; true),
+    (nb_current(pals_filtrados, _) -> nb_delete(pals_filtrados) ; true).
