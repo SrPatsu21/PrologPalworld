4766664cf967cd1ed5dabdbded6d59d559bdfbd1
paulo <paulosergiopierdona@gmail.com>
2025-05-26 10:28:37 -0300

    new structure

---
+++ base_palworld.pl
@@ -0,0 +1,16 @@
+% BASE DE DADOS
+% pal(Número, Nome, Tipos, Trabalhos, Montaria, Passivas).
+pal(1, lamball, [neutro], [trabalho_manual, transporte, agricultura], nao, [fluffy_shield]).
+pal(2, cattiva, [neutro], [trabalho_manual, transporte, coleta, mineracao], nao, [cat_helper]).
+pal(3, chikipi, [neutro], [coleta, agricultura], nao, [egg_layer]).
+pal(4, lifmunk, [grama], [plantio, trabalho_manual, corte, producao_medicinal, coleta], nao, [lifmunk_recoil]).
+pal(5, foxparks, [fogo], [acendimento], nao, [foxparks_partner]).
+pal(20, rushoar, [terra], [mineracao], sim, [hard_head]).
+pal(26, direhowl, [neutro], [transporte], sim, [direhowl_rider]).
+pal(33, mossanda, [grama], [corte, transporte], sim, [grenadier_panda]).
+pal(47, galeclaw, [vento], [transporte], sim, [galeclaw_rider]).
+pal(61, kitsun, [fogo], [acendimento], sim, [clear_mind]).
+pal(65, surfent, [agua], [transporte], sim, [swift_swimmer]).
+pal(90, mammorest, [terra], [corte, mineracao], sim, [gaia_crusher]).
+pal(103, grizzbolt, [eletrico], [geracao_eletricidade, trabalho_manual, transporte, corte], sim, [yellow_tank]).
+pal(110, jetragon, [dragao], [coleta], sim, [aerial_missile]).
