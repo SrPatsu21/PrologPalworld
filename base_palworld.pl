e6575547a68132814e94b691ed98f58ee64b91da
SrPatsu21 <paulosergiopierdona@gmail.com>
2025-05-27 23:06:15 -0300

    start db

--- base_palworld.pl
+++ base_palworld.pl
@@ -2,14 +2,28 @@
 % ref: <https://palworld.gg/pals>
 % pal(Número, Nome, [Tipos], Habilidade, [Trabalhos], [Drops], VidaBase, AtaqueBase, DefesaBase, Montaria).
 
-pal(001, lamball, [normal], cute_charm, [medicine_production, transport, handiwork], [wool], 120, 80, 90, nao_montaria).
-pal(002, cattiva, [dark], intimidation, [transport, handiwork], [leather], 110, 95, 85, nao_montaria).
-pal(003, chikipi, [normal], egg_barrage, [gathering], [egg], 100, 70, 60, nao_montaria).
-pal(004, lifmunk, [grass], seed_machine_gun, [handiwork, logging], [lifmunk_effigy], 130, 85, 95, nao_montaria).
-pal(005, foxparks, [fire], flame_thrower, [kindling], [flame_organ], 115, 90, 80, montaria).
-pal(006, fuack, [water], hydro_jet, [watering], [poultry_meat], 125, 75, 70, nao_montaria).
-pal(007, sparkit, [electric], electric_ball, [generating_electricity], [electric_organ], 105, 100, 65, nao_montaria).
-pal(008, tinkaton, [ground], hammer_smash, [mining], [stone], 140, 110, 100, nao_montaria).
-pal(009, cremerry, [ice], frost_shard, [cooling], [ice_organ], 95, 60, 55, nao_montaria).
-pal(010, roselia, [grass, poison], thorn_whip, [medicine_production], [poisonous_thorn, abc1, abc2], 110, 85, 90, montaria).
-pal(011, roselia2, [grass, poison], thorn_whip, [medicine_production], [poisonous_thorn, abc1, abc2], 110, 85, 90, montaria).
+pal(001, Lamball, [Normal], Fluffy_Shield, [Handiwork, Farming, Transporting], [Wool, Lamball_Mutton], 70, 70, 70, nao_montaria).
+pal(002, Cattiva, [Normal], Cat_Helper, [Gathering, Handiwork, Mining, Transporting], [Red_Berries], 70, 70, 70, nao_montaria).
+pal(003, Chikipi, [Normal], Egg_Layer, [Gathering, Transporting], [Egg, Chikipi_Poultry], 60, 60, 70, nao_montaria).
+pal(004, Lifmunk, [Leaf], Lifmunk_Recoil, [Gathering, Deforesting, Handiwork, Medicine_Production, Planting], [Berry_Seeds, Low_Grade_Medical_Supplies], 75, 70, 70, nao_montaria).
+pal(005, Foxparks, [Fire], Huggy_Fire, [Kindling], [Leather, Flame_Organ], 65, 70, 75, nao_montaria).
+pal(005, Foxparks_Cryst, [Ice], Huggy_Frost, [Cooling], [Leather, Ice_Organ], 65, 70, 80, nao_montaria).
+pal(006, Fuack, [Water], Surfing_Slam, [Handiwork, Transporting, Watering], [Leather, Pal_Fluids], 60, 60, 80, nao_montaria).
+pal(007, Sparkit, [Electricity], Static_Electricity, [Generating_Electricity, Handiwork, Transporting], [Electric_Organ], 60, 70, 75, nao_montaria).
+pal(008, Tanzee, [Leaf], Cheery_Rifle, [Gathering, Deforesting, Watering, Handiwork, Planting, Transporting], [Mushroom], 80, 70, 100, nao_montaria).
+pal(009, Rooby, [Fire], Tiny_Spark, [Kindling], [Flame Organ, Leather], 75, 75, 100, nao_montaria).
+pal(010, Pengullet, [Water, Ice], Pengullet_Cannon, [Cooling, Handiwork, Transporting, Watering], [Ice Organ, Pal Fluids], 70, 70, 75, nao_montaria).
+pal(011, Penking, [Water, Ice], Brave_Sailor, [Cooling, Handiwork, Mining, Transporting, Watering], [Ice Organ, Pal Fluids], 95, 95, 95, nao_montaria).
+pal(012, Jolthog, [Electricity], Jolt_Bomb, [Generating_Electricity], [Electric_Organ], 70, 70, 75, nao_montaria).
+pal(012, Jolthog_Cryst, [Ice], Cold_Bomb, [Cooling], [Ice_Organ], 70, 80, 75, nao_montaria).
+pal(013, Gumoss, [Leaf, Earth], Logging_Assistance, [Planting], [Berry_Seeds, Gumoss_Leaf], 70, 80, 100, nao_montaria).
+pal(014, Vixy, [Normal], Dig_Here, [Gathering, Farming], [Leather, Bone], 70, 70, 70, nao_montaria).
+pal(015, Hoocrates, [Dark], Dark_Knowledge, [Gathering], [Fiber, High_Grade_Technical_Manual], 70, 80, 70, nao_montaria).
+pal(016, Teafant, [Water], Soothing_Shower, [Watering], [Pal_Fluids], 70, 70, 70, nao_montaria).
+pal(017, Depresso, [Dark], Caffeine_Inoculation, [Handiwork, Mining, Transporting], [Venom_Gland], 70, 70, 70, nao_montaria).
+pal(018, Cremis, [Normal], Fluffy_Wool, [Gathering, Farming], [Wool], 70, 75, 100, nao_montaria).
+pal(019, Daedream, [Dark], Dream_Chaser, [Gathering, Handiwork, Transporting], [Venom_Gland, Small_Pal_Soul], 80, 60, 100, nao_montaria).
+pal(020, Rushoar, [Earth], Hard_Head, [Mining], [Rushoar_Pork, Leather, Bone], 80, 70, 100, montaria).
+pal(021, Nox, [Dark], Kuudere, [Gathering], [Leather, Small_Pal_Soul], 75, 70, 85, nao_montaria).
+
+
