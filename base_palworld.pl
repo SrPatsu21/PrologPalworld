47c2b85f0e32cfc0ac02b0a956d926cdc2ef5bc0
SrPatsu21 <paulosergiopierdona@gmail.com>
2025-05-28 14:27:27 -0300

    quase metade dos pal ta ai

--- base_palworld.pl
+++ base_palworld.pl
@@ -25,5 +25,61 @@ pal(018, cremis, [normal], fluffy_wool, [gathering, farming], [wool], 70, 75, 10
 pal(019, daedream, [dark], dream_chaser, [gathering, handiwork, transporting], [venom_gland, small_pal_soul], 80, 60, 100, nao_montaria).
 pal(020, rushoar, [earth], hard_head, [mining], [rushoar_pork, leather, bone], 80, 70, 100, montaria).
 pal(021, nox, [dark], kuudere, [gathering], [leather, small_pal_soul], 75, 70, 85, nao_montaria).
+pal(022, fuddler, [earth], ore_detector, [handiwork, mining, transporting], [leather], 65, 50, 100, nao_montaria).
+pal(023, killamari, [dark], fried_squid, [gathering, transporting], [venom_gland, killamari_tentacle], 60, 70, 100, nao_montaria).
+pal(024, mau, [dark], gold_digger, [farming], [gold_coin], 70, 70, 70, nao_montaria).
+pal(024, mau_cryst, [ice], gold_digger, [cooling, farming], [ice_organ, sapphire], 70, 70, 70, nao_montaria).
+pal(025, celaray, [water], zephyr_glider, [transporting, watering], [pal_fluids], 80, 80, 100, nao_montaria).
+pal(026, direhowl, [normal], direhowl_rider, [gathering], [leather, ruby, gold_coin], 80, 75, 110, montaria).
+pal(027, tocotoco, [normal], eggbomb_launcher, [gathering], [gunpowder, tocotoco_feather], 60, 70, 80, nao_montaria).
+pal(028, flopie, [leaf], helper_bunny, [gathering, handiwork, medicine_production, planting, transporting], [low_grade_medical_supplies, wheat_seeds, potato_seeds], 60, 70, 80, nao_montaria).
+pal(029, mozzarina, [normal], milk_maker, [farming], [mozzarina_meat, milk], 90, 80, 100, nao_montaria).
+pal(030, bristla, [leaf], princess_gaze, [gathering, handiwork, medicine_production, planting, transporting], [wheat_seeds, lettuce_seeds, carrot_seeds], 80, 80, 80, nao_montaria).
+pal(031, gobfin, [water], angry_shark, [handiwork, transporting, watering], [pal_fluids], 90, 75, 90, nao_montaria).
+pal(031, gobfin_ignis, [fire], angry_shark, [handiwork, transporting, kindling], [gobfin_ignis], 90, 75, 90, nao_montaria).
+pal(032, hangyu, [earth], flying_trapeze, [gathering, handiwork, transporting], [fiber], 80, 70, 70, nao_montaria).
+pal(032, hangyu_cryst, [ice], winter_trapeze, [gathering, cooling, handiwork, transporting], [fiber, ice_organ], 80, 70, 80, nao_montaria).
+pal(033, mossanda, [leaf], grenadier_panda, [deforesting, handiwork, planting, transporting], [mushroom, leather, tomato_seeds, onion_seeds], 100, 90, 100, montaria).
+pal(033, mossanda_lux, [electricity], grenadier_panda, [deforesting, handiwork, generating_electricity, transporting], [mushroom, leather, electric_organ], 100, 100, 100, montaria).
+pal(034, woolipop, [normal], candy_pop, [farming], [cotton_candy, high_quality_pal_oil], 70, 90, 70, nao_montaria).
+pal(035, caprity, [leaf], berry_picker, [farming, planting], [caprity_meat, red_berries, horn], 100, 90, 70, nao_montaria).
+pal(035, caprity_noct, [dark], venom_picker, [farming, planting], [caprity_meat, red_berries, horn, venom_gland], 100, 90, 75, nao_montaria).
+pal(036, melpaca, [normal], pacapaca_wool, [farming], [wool, leather], 90, 90, 90, montaria).
+pal(037, eikthyrdeer, [normal], guardian_of_the_forest, [deforesting], [eikthyrdeer_venison, horn], 95, 80, 80, montaria).
+pal(038, nitewing, [normal], travel_companion, [gathering], [leather], 100, 80, 100, montaria).
+pal(039, ribbuny, [normal], skilled_fingers, [gathering, handiwork, transporting], [leather, beautiful_flower, ribbuny_ribbon], 75, 70, 100, nao_montaria).
+pal(039, ribbuny_botan, [leaf], grass_weaver, [gathering, handiwork, planting, transporting], [leather, beautiful_flower, ribbuny_ribbon, potato_seeds], 80, 70, 100, nao_montaria).
+pal(040, incineram, [fire, dark], flameclaw_hunter, [kindling, handiwork, mining, transporting], [horn, leather], 95, 85, 150, nao_montaria).
+pal(040, incineram_noct, [dark], darkclaw_hunter, [handiwork, mining, transporting], [horn, leather], 95, 85, 150, nao_montaria).
+pal(041, cinnamoth, [leaf], mysterious_scales, [medicine_production, planting], [honey, lettuce_seeds, wheat_seeds, onion_seeds], 70, 80, 100, nao_montaria).
+pal(042, arsox, [fire], warm_body, [deforesting, kindling], [horn, flame_organ], 85, 95, 100, montaria).
+pal(043, dumud, [earth], soil_improver, [mining, farming, transporting, watering], [raw_dumud, high_quality_pal_oil], 100, 95, 100, montaria).
+pal(044, cawgnito, [dark], telepeck, [deforesting], [bone, venom_gland, small_pal_soul], 75, 80, 95, nao_montaria).
+pal(045, leezpunk, [dark], sixth_sense, [gathering, handiwork, transporting], [copper_key, silver_key, leezpunk_crest], 80, 50, 90, nao_montaria).
+pal(045, leezpunk_ignis, [fire], sixth_sense, [gathering, kindling, handiwork, transporting], [flame_organ, silver_key, leezpunk_crest], 80, 50, 90, nao_montaria).
+pal(046, loupmoon, [dark], dark_gleam_strike, [handiwork], [bone], 80, 80, 130, nao_montaria).
+pal(046, loupmoon_cryst, [ice], frozen_gleam_strike, [cooling, handiwork], [bone], 80, 80, 130, nao_montaria).
+pal(047, galeclaw, [normal], frozen_gleam_strike, [gathering], [galeclaw_poultry, leather], 75, 60, 120, nao_montaria).
+pal(048, robinquill, [leaf], hawk_eye, [gathering, deforesting, handiwork, medicine_production, planting, transporting], [wheat_seeds, arrow, potato_seeds], 90, 80, 105, nao_montaria).
+pal(048, robinquill_terra, [leaf, earth], hawk_eye, [gathering, deforesting, handiwork, medicine_production, planting, transporting], [wheat_seeds, arrow, potato_seeds], 90, 80, 105, nao_montaria).
+pal(049, gorirat, [normal], full-power_gorilla_mode, [deforesting, handiwork, transporting], [leather, bone], 90, 90, 110, nao_montaria).
+pal(049, gorirat_terra, [earth], full-power_gorilla_mode, [handiwork, mining, transporting], [ore, bone], 90, 90, 110, nao_montaria).
+pal(050, beegarde, [leaf], worker_bee, [gathering, deforesting, handiwork, farming, medicine_production, planting, transporting], [honey], 80, 90, 100, nao_montaria).
+pal(051, elizabee, [leaf], queen_bee_command, [gathering, deforesting, handiwork, medicine_production, planting], [honey, elizabees_staff], 90, 100, 105, nao_montaria).
+pal(052, grintale, [normal], plump_body, [gathering], [high_quality_pal_oil], 110, 80, 100, montaria).
+pal(053, swee, [ice], fluffy, [gathering, cooling], [wool, swee_hair], 60, 60, 100, nao_montaria).
+pal(054, sweepa, [ice], king_of_fluff, [gathering, cooling], [wool], 100, 90, 100, montaria).
+pal(055, chillet, [ice, dragon], wriggling_weasel, [gathering, cooling], [leather], 90, 80, 100, montaria).
+pal(055, chillet_ignis, [fire, dragon], sparkling_weasel, [gathering, kindling], [leather, flame_organ], 90, 80, 100, montaria).
+pal(056, univolt, [electricity], swift_deity, [deforesting, generating_electricity], [leather, electric_organ, horn], 80, 105, 105, montaria).
+pal(057, foxcicle, [ice], aurora_guide, [cooling], [leather, ice_organ], 90, 105, 100, nao_montaria).
+pal(058, pyrin, [fire], red_hare, [deforesting, kindling], [flame_organ, leather], 100, 90, 110, montaria).
+pal(058, pyrin_noct, [fire, dark], red_hare, [deforesting, kindling], [flame_organ, leather], 100, 90, 110, montaria).
+pal(059, reindrix, [ice], cool_body, [cooling, deforesting], [reindrix_venison, leather, horn, ice_organ], 100, 110, 85, montaria).
+
+
+
+
+
 
 
